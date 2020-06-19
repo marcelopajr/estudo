9c93a25c8bd856d483b0efdae896fe5e5c8ef87d
Marcelo Pereira <marcelo96jr@gmail.com>
2020-06-18 22:06:34 -0300

    Condições

---
+++ js-curso-em-video/ex08.js
@@ -0,0 +1,10 @@
+var vel = 80
+
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+
+// CONDIÇÃO SIMPLES
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Digija sempre usando cinto de segurança!`)
---
+++ js-curso-em-video/ex09.js
@@ -0,0 +1,10 @@
+var pais = "EUA"
+
+console.log(`Vivendo em ${pais}`)
+
+// CONDIÇÃO COMPOSTA
+if (pais == "Brasil") {
+    console.log("Você é brasileiro!")
+} else {
+    console.log("Você é estrangeiro!")
+}
