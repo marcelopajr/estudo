f9e641f5c54e8770f4bb97b0076b0233d8df91dd
marcelopajr <marcelo96jr@gmail.com>
2021-07-23 17:21:23 -0300

    Aula tudo-e-objeto finalizada

---
+++ javascript/javascript-completo-origamid/javascript-para-iniciantes/tudo-e-objeto/script-aula.js
@@ -0,0 +1,6 @@
+var nome = 'Marcelo';
+
+var nomeMinusculo = nome.toLowerCase();
+
+var btn = document.querySelector('.btn');
+console.log(btn.innerText);
---
+++ javascript/javascript-completo-origamid/javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Marcelo';
+// toLowerCase
+// toUpperCase
+// length
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+// scroll
+// before
+// appendChild
+// innerHTML
+// id
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
