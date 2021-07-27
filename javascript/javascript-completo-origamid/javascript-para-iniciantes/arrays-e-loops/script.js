86a4d14b23768a5b820b1a21e1577655bf7fae44
marcelopajr <marcelo96jr@gmail.com>
2021-07-27 10:49:21 -0300

    Fim da aula de Arrays e Loops

--- javascript/javascript-completo-origamid/javascript-para-iniciantes/arrays-e-loops/script.js
+++ javascript/javascript-completo-origamid/javascript-para-iniciantes/arrays-e-loops/script.js
@@ -1,27 +1,24 @@
-var videoGames = ['Switch', 'PS4', 'Xbox'];
-console.log(videoGames[0]);
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilWin = ['1959', '1962', '1970', '1994', '2002'];
 
-// videoGames.pop();
-// console.log(videoGames);
-videoGames.push('3DS');
-console.log(videoGames);
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+brasilWin.forEach(function (ano) {
+  console.log(`O Brasil ganhou a copa de ${ano}`);
+});
 
-// For loop
-for (var num = 0; num <= 3; num++) {
-  console.log(num);
-}
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
 
-// While loop
-var i = 5;
-while (i <= 25) {
-  console.log(i);
-  i += 5;
-}
-
-for (var item = 0; item < videoGames.length; item++) {
-  console.log(videoGames[item]);
-
-  if (videoGames[item] === 'PS4') {
+for (i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+  if (frutas[i] === 'Pera') {
     break;
   }
 }
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+var ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta);
